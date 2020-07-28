3c9a083bae0db03866fa216b9526a7ef8ce55b73
Everton Nascimento <evertonflbruce@gmail.com>
2020-07-28 18:17:39 -0300

    Formulário de cadastro de categoria armazenando dados.

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,11 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PagerDefaut from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
 
+function useFormik({
+    initialValues
+}) {
+    const [values, setValues] = useState(initialValues);
+
+    function handleChange(event) {
+        const fieldName = event.target.getAttribute('name');
+        const value = event.target.value;
+        setValues({
+            ...values,
+            [fieldName]: value,
+        });
+    }
+
+    return {
+        values,
+        handleChange
+    }
+}
+
 function CadastroCategoria() {
+    const formik = useFormik({
+        initialValues: {
+            nomeCategoria: '',
+            descricaoCategoria: '',
+        }
+    });
+
     return (
         <PagerDefaut>
             <h1>Cadastro de Categoria</h1>
+
+            <form onSubmit={(event) => {
+                event.preventDefault();
+                window.alert(`Dados prontos para serem inseridos!`)
+            }}>
+                <input
+                    className="name"
+                    type="text"
+                    placeholder="Nome"
+                    name="nomeCategoria"
+                    id="nomeCategoria"
+                    onChange={formik.handleChange}
+                    value={formik.values.nomeCategoria}
+                /><br />
+
+
+                <input
+                    className="description"
+                    type="text"
+                    placeholder="Descrição"
+                    name="descricaoCategoria"
+                    id="descricaoCategoria"
+                    onChange={formik.handleChange}
+                    value={formik.values.descricaoCategoria}
+                /> <br />
+
+                <button type="submit">
+                    Cadastrar
+                </button>
+
+                <button className="limpar" type="reset">
+                    Limpar
+                </button>
+            </form><br /><br /><br />
 
             <Link to="/">
                 Home
